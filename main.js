c23f195fa0bd3f12165f0e146459ca9f06b0a96e
Fareszran <frareszran11@gmail.com>
2023-11-23 10:06:12 +0100

    100% done

--- main.js
+++ main.js
@@ -2,7 +2,28 @@ $(document).ready(function(){
     $(".btn.btn-primary").click(function(){
         window.location.href = 'http://127.0.0.1:5500/More%20Destination.html';
     });
-    $(".navbar-link").click(function(){
+    $("#k").click(function(){
         window.location.href = 'http://127.0.0.1:5500/contactUs.html';
     });
 });
+$(document).ready(function(){
+    $(".btn.btn-primary").click(function(){
+        window.location.href = 'Book Now.html';
+    })
+    });$(document).ready(function(){
+        $(".btn.btn-secondary").click(function(){
+            window.location.href = 'Book Now.html';
+        })
+        });$(document).ready(function(){
+            $(".btn.btn-primary").click(function(){
+                window.location.href = 'Book Now.html';
+            })
+            });$(document).ready(function(){
+                $(".btn.btn-secondary").click(function(){
+                    window.location.href = 'Book Now.html';
+                })
+                });$(document).ready(function(){
+                    $(".btn.btn-hi").click(function(){
+                        window.location.href = 'View All Packages.html';
+                    })
+                    });
