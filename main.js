a0239f2a84cfa63e87439c97d25658908248ceba
khalilcherni <kcherni411@gmail.com>
2023-11-23 12:47:36 +0000

    15

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".btn.btn-pri").click(function(){
+    $(".btn btn-pri").click(function(){
         window.location.href = 'http://127.0.0.1:5500/More%20Destination.html';
     });
     $("#k").click(function(){
@@ -45,7 +45,16 @@ $(document).ready(function(){
         
    })  
 
-                    });$(document).ready(function(){
+                    ;$(document).ready(function(){
                         $(".btn.btn-secondaryd").click(function(){
-                            window.location.href = 'Book Now.html';
+                            window.location.href = 'http://127.0.0.1:5500/Book%20Now.html';
                         })})
+                        $("#fb").click(function(){
+                            window.location.href = 'https://www.facebook.com/VisitTUN';
+                        })
+                        $("#twitter").click(function(){
+                            window.location.href = 'https://twitter.com/Visit_Tunisia';
+                        })
+                        $("#youtube").click(function(){
+                            window.location.href = 'https://youtu.be/21r1M3L-yqM?si=aQvsfOKYlX4GDJtH';
+                        })
